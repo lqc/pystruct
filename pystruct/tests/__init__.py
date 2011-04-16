815adb4294e6cf121e959297c05aa0e9b85750b2
Łukasz Rekucki <lrekucki@gmail.com>
2011-04-16 14:34:53 +0200

    Extra tests.

--- pystruct/tests/__init__.py
+++ pystruct/tests/__init__.py
@@ -1,3 +1,4 @@
 from pystruct.tests.test_numeric import NumericFieldTest
 from pystruct.tests.test_strings import StringFieldTest
 from pystruct.tests.test_complex import ArrayFieldTest, StructFieldTest
+from pystruct.tests.test_struct import StructTest
---
+++ pystruct/tests/test_struct.py
@@ -0,0 +1,13 @@
+from pystruct.utils import unittest
+from pystruct import CStruct, CField
+
+class Dummy(CStruct):
+    field = CField(0)
+
+class StructTest(unittest.TestCase):
+
+    def test_missing_argument(self):
+        self.assertRaises(ValueError, Dummy)
+
+    def test_unresolved_argument(self):
+        self.assertRaises(ValueError, Dummy, field="F", extra="E")
